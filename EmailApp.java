3eab8a048ab1c6fd79f7826aaca6f21e264e4c4a
Zachery Vitko <zwvitko1@gmail.com>
2020-11-25 01:09:43 -0500

    Add files via upload

---
+++ EmailApp.java
@@ -0,0 +1,11 @@
+package emailapp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		Email email1 = new Email("Zach", "Vitko");
+
+		System.out.println(email1.showInfo());
+	}
+
+}
